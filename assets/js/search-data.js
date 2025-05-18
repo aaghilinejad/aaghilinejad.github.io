da4ab8f7cff77b8c8a96ac2d4fc79e1bad75d2da
aaghilinejad <43452463+aaghilinejad@users.noreply.github.com>
2025-05-18 01:15:58 +0000

    Deploying to gh-pages from @ aaghilinejad/aaghilinejad.github.io@8fadb179896657cf3355783bb18d06c42adc660d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,12 +23,12 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+        },{id: "nav-research",
+          title: "Research",
+          description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/repositories/";
+            window.location.href = "/teaching/";
           },
         },{id: "books-the-godfather",
           title: 'The Godfather',
