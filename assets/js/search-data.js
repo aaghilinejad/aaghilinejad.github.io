f571b3e2ceaf437318e21e082a94c4566f2e7b32
aaghilinejad <43452463+aaghilinejad@users.noreply.github.com>
2025-05-18 19:33:01 +0000

    Deploying to gh-pages from @ aaghilinejad/aaghilinejad.github.io@3307365ec10fb49a91111fd4f87e384f13ebf3e5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,6 +30,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
+        },{id: "nav-projects",
+          title: "Projects",
+          description: "A growing collection of your cool projects.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
         },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
