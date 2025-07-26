f16609dd565d7dd48b8dc115454e12ccb412224a
Arian Aghilinejad <arian.aghili1298@gmail.com>
2025-07-26 10:00:29 -0700

    commit dark

--- _scripts/search.liquid.js
+++ _scripts/search.liquid.js
@@ -322,7 +322,7 @@ ninja.data = [
       description: 'Change the theme of the site to System Default',
       section: 'Theme',
       handler: () => {
-        setThemeSetting("system");
+        setThemeSetting("dark"); 
       },
     },
   {%- endif -%}
